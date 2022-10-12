956d03e4d51d74af5078e08e5c1aea9998851f4b
Valereon <tools418@gmail.com>
2022-10-12 12:04:19 -0400

    Nightly commit 2022-10-12

--- JavaGame/Classes/CraftingRecipes.java
+++ JavaGame/Classes/CraftingRecipes.java
@@ -2,21 +2,6 @@ package JavaGame.Classes;
 
 //crafting recipes for all craftable items
 class CraftingRecipes{
-hkjnibgrehibfebdayhibtegahibefrtgahbirgebhirgebhnithjhhitjithjrijthijhtijhttjiijhthitjihtjjithjihthtijrt[i[tihojjihtrthrjiotrijo]]
-
-
-
-
-
-
-//commit test and stuff
-
-
-
-
-
-
-//commit test and stuff
 
 
 
